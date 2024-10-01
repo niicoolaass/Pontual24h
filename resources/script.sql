992b41cd61417269710967e7b76f507fadf411cd
niicoolaass <nicolasvieira147147@hotmail.com>
2024-10-01 20:02:46 -0300

    projeto

---
+++ resources/script.sql
@@ -0,0 +1,6 @@
+create database pontual24h;
+
+create table funcionarios{
+    id int not null auto_increment primary key,
+
+}
---
+++ src/br/com/ponto/model/Funcionarios.java
@@ -0,0 +1,4 @@
+package br.com.ponto.model;
+
+public class Funcionarios {
+}
