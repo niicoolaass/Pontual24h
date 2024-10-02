79759f532954a41449cf71361323ef8bfc757405
niicoolaass <nicolasvieira147147@hotmail.com>
2024-10-02 19:59:23 -0300

    conecction

--- src/br/com/ponto/factory/ConnectionFactory.java
+++ src/br/com/ponto/factory/ConnectionFactory.java
@@ -1,4 +1,35 @@
 package br.com.ponto.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3307/pontual24h";
+
+    // Conexão com o banco de dados
+    public static Connection createConnectionToMySQL() throws  Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+
+        return  connection;
+    }
+
+    public static void main(String[] args) {
+
+        // Recuperar uma conexão com o banco de dados
+        Connection con = createConnectionToMySQL();
+
+        //Teste se a conexão é nula
+        if (con!=null) {
+            System.out.println("Conexão obetida com sucesso!");
+            con.close();
+        }
+    }
+
+
+
 }
