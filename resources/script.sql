e66630679dbc3545075d14ff2ad9871e16cf07a9
niicoolaass <nicolasvieira147147@hotmail.com>
2024-10-01 22:49:58 -0300

    att

--- resources/script.sql
+++ resources/script.sql
@@ -1,6 +1,10 @@
 create database pontual24h;
 
-create table funcionarios{
-    id int not null auto_increment primary key,
-
-}
+create table funcionarios(
+    id_funcionario int not null auto_increment primary key,
+    nome varchar(40),
+    data_nascimento date,
+    cargo varchar(40),
+    dataAdmissao date,
+    status varchar
+);
--- src/br/com/ponto/model/Funcionarios.java
+++ src/br/com/ponto/model/Funcionarios.java
@@ -1,7 +1,51 @@
 package br.com.ponto.model;
 
+import java.util.Date;
+
 public class Funcionarios {
-    public static void main(String[] args) {
-        System.out.println("oii");
+    private int id;
+    private String nome;
+    private String cargo;
+    private Date dataAdmissao;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
